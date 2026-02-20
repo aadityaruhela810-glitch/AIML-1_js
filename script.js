e29b46896299ddee8f374a37d53839ef3025aa87
unknown <aadityaruhela810@gmail.com>
2026-02-20 10:32:32 +0530

    script

---
+++ script.js
@@ -0,0 +1,51 @@
+// console.log("Hello world")
+// document.write("Hello world")
+
+// var num =10;
+// console.log(num);
+// console.log(typeof(num));
+// num =false;
+// console.log(typeof(num));
+// num="sanjeev.son";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+
+
+// //function 
+// var sayHello=function 0 {
+//     alert("Hello world");
+// }
+
+// //array 
+// var numberArray=[1,2,3];
+// var animal=new Array("cat","dog");
+
+// //object 
+// var person={
+//     name: "aaditya ",
+//     age: "18",
+//     tittle:"college student"
+// }
+
+// //symbol 
+// const syml = symbol{4}
+// console.log(syml);
+// const sym2 = symbol(4);
+// console.log(sym2);
+// if(sym1=sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false")
+// }
+
+var a=100;var b=13; var c=10; var linebreak = "<br>";
+
+document.write("a+b+c=");
+result = a+b+c;
+document.write(result);
+document.write(linebreak);
